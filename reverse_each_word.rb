6a91675664d127f2e8fbf27b525b2aedcc827c42
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 23:04:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
-  reverse_each_word(string).each do |reversal|
-    return reversal
+  string.split(" ")
+  string.each do |reversal|
+    reversal.reverse
   end
-  return "#{reversal.reverse}"
 end
